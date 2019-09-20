e3d45f5170124c82298def1e42d37cf6cf328363
Ahmet Uyar <4hmetuyar@gmail.com>
2019-09-20 21:45:04 +0300

    Issue & Base Entity File added.

--- src/main/java/com/uyar/issuemanagement/entity/BaseEntity.java
+++ src/main/java/com/uyar/issuemanagement/entity/BaseEntity.java
@@ -1,4 +1,26 @@
 package com.uyar.issuemanagement.entity;
 
-public class BaseEntity {
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@MappedSuperclass
+public abstract class BaseEntity implements Serializable {
+    @Column(name = "created_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date CreatedAt;
+    @Column(name = "created_by", length = 100)
+    private String CreatedBy;
+    private Date UpdatedAt;
+    @Column(name = "updated_by", length = 100)
+    private String UpdatedBy;
+    @Column(name = "status")
+    private Boolean Status;
+
 }
--- src/main/java/com/uyar/issuemanagement/entity/Issue.java
+++ src/main/java/com/uyar/issuemanagement/entity/Issue.java
@@ -1,4 +1,19 @@
 package com.uyar.issuemanagement.entity;
 
-public class Issue {
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "issue")
+public class Issue extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "description", length = 400)
+    private String description;
+    @Column(name = "date")
+    private Date date;
+    @Column(name = "issue_status")
+    @Enumerated(EnumType.STRING)
+    private IssueStatus issueStatus;
 }
