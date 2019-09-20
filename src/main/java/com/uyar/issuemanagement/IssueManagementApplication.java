ad8ff30d79e2c4270a00e21eb1ec7a59362a5e28
Ahmet Uyar <4hmetuyar@gmail.com>
2019-09-20 19:05:32 +0300

    initial commit

---
+++ src/main/java/com/uyar/issuemanagement/IssueManagementApplication.java
@@ -0,0 +1,13 @@
+package com.uyar.issuemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueManagementApplication.class, args);
+	}
+
+}
