189d56f507dc53619551108f500f74b162188cc0
Ahmet Uyar <4hmetuyar@gmail.com>
2019-09-22 20:48:23 +0300

    Service Impl file added.

---
+++ src/main/java/com/uyar/issuemanagement/service/impl/ProjectServiceImpl.java
@@ -0,0 +1,103 @@
+package com.uyar.issuemanagement.service.impl;
+
+import com.uyar.issuemanagement.dto.ProjectDto;
+import com.uyar.issuemanagement.entity.Project;
+import com.uyar.issuemanagement.entity.User;
+import com.uyar.issuemanagement.repository.ProjectRepository;
+import com.uyar.issuemanagement.repository.UserRepository;
+import com.uyar.issuemanagement.service.ProjectService;
+import com.uyar.issuemanagement.util.TPage;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class ProjectServiceImpl implements ProjectService {
+
+    private final ProjectRepository projectRepository;
+    private final ModelMapper modelMapper;
+    private final UserRepository userRepository;
+
+    public ProjectServiceImpl(ProjectRepository projectRepository, UserRepository userRepository, ModelMapper modelMapper) {
+        this.projectRepository = projectRepository;
+        this.modelMapper = modelMapper;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public ProjectDto save(ProjectDto project) {
+
+        Project projectCheck = projectRepository.getByProjectCode(project.getProjectCode());
+
+        if (projectCheck != null)
+            throw new IllegalArgumentException("Project Code Already Exist");
+
+        Project p = modelMapper.map(project, Project.class);
+        User user = userRepository.getOne(project.getManagerId());
+        p.setManager(user);
+
+        p = projectRepository.save(p);
+        project.setId(p.getId());
+        return project;
+    }
+
+    @Override
+    public ProjectDto getById(Long id) {
+        Project p = projectRepository.getOne(id);
+        return modelMapper.map(p, ProjectDto.class);
+    }
+
+    @Override
+    public ProjectDto getByProjectCode(String projectCode) {
+        return null;
+    }
+
+    @Override
+    public List<ProjectDto> getByProjectCodeContains(String projectCode) {
+        return null;
+    }
+
+    @Override
+    public TPage<ProjectDto> getAllPageable(Pageable pageable) {
+        Page<Project> data = projectRepository.findAll(pageable);
+        TPage<ProjectDto> respnose = new TPage<ProjectDto>();
+        respnose.setStat(data, Arrays.asList(modelMapper.map(data.getContent(), ProjectDto[].class)));
+        return respnose;
+    }
+
+    @Override
+    public Boolean delete(ProjectDto project) {
+        return null;
+    }
+
+    public Boolean delete(Long id) {
+        projectRepository.deleteById(id);
+        return true;
+    }
+
+    @Override
+    public ProjectDto update(Long id, ProjectDto project) {
+        Project projectDb = projectRepository.getOne(id);
+        if (projectDb == null)
+            throw new IllegalArgumentException("Project Does Not Exist ID:" + id);
+
+        Project projectCheck = projectRepository.getByProjectCodeAndIdNot(project.getProjectCode(), id);
+        if (projectCheck != null)
+            throw new IllegalArgumentException("Project Code Already Exist");
+
+        projectDb.setProjectCode(project.getProjectCode());
+        projectDb.setProjectName(project.getProjectName());
+
+        projectRepository.save(projectDb);
+        return modelMapper.map(projectDb, ProjectDto.class);
+    }
+
+    public List<ProjectDto> getAll() {
+        List<Project> data = projectRepository.findAll();
+        return Arrays.asList(modelMapper.map(data, ProjectDto[].class));
+    }
+}
